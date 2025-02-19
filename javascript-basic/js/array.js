f4b30cac6baac0edfe58ddcaa722ab2086a330d0
nakano-hiro <monsto.nakano@gmail.com>
2025-02-19 11:40:02 +0900

    010を追加

---
+++ javascript-basic/js/array.js
@@ -0,0 +1,20 @@
+// 配列の宣言と値の代入を行う
+const userNames = ['太郎','一郎','次郎','三郎','四郎'];
+
+// 配列の値を出力する
+console.log(userNames);
+
+// 2番目の要素を更新する
+userNames[1] = '花子';
+
+// 6番目に要素を追加する
+userNames[5] = '五郎';
+
+// 配列の値を出力する
+console.log(userNames);
+
+// 3番目の要素を出力する
+console.log(userNames[2]);
+
+// 配列の要素数を出力する
+console.log(userNames.length);
